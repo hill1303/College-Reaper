4d92b1cdea8994ad0508e071ff2646d035b0f7e2
gaurimishra <gaurimishra@users.noreply.github.com>
2014-07-17 15:04:45 -0400

    Commit Migrations

--- db/migrate/20140717151003_create_colleges.rb
+++ db/migrate/20140717151003_create_colleges.rb
@@ -1,9 +1,11 @@
 class CreateColleges < ActiveRecord::Migration
   def change
     create_table :colleges do |t|
+
+    	# College name
     	t.string :name
 
-    	#Metadata field
+    	# Metadata field
     	t.timestamps
     end
   end
--- db/migrate/20140717155738_create_terms.rb
+++ db/migrate/20140717155738_create_terms.rb
@@ -1,9 +1,14 @@
 class CreateTerms < ActiveRecord::Migration
   def change
     create_table :terms do |t|
+    	
+    	# General Term info
     	t.string :name
     	t.date :start
     	t.date :end
+
+    	# Metadata field
+    	t.timestamps
     end
   end
 end
