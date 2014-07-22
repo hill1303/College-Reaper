94bfdd5c27ce4f3a82be2c5cca84b027db4406ea
eaholt06 <eaholt06@users.noreply.github.com>
2014-07-22 15:06:40 -0400

    Fixes documentation issues in course.rb and location .rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,13 +1,16 @@
 # The +Course+ class provides information about an individual course. 
-# Each course is related to a Course Group, College and Term. After 
+# Each course is related to a Course Group, Section and Term. After 
 # a user enters their preferences, courses will be evaluated and eliminated.
 # Course information will also be displayed to the user once a schedule is
 # determined.
 #
-# +:courseNo+ is the four digit course number for a specific course
-# +:instrUnit+ represents the acronym that procedes a course number
-# +:name+ provides a descriptive title for the course
-# +:creditHrs+ provides the number of hours counted toward degree requirements
+# Attributes are stored as followed:
+#
+#   * +:course_number+ is the four digit course number for a specific course
+#   * +:instructional_unit+ represents the acronym that procedes a course number
+#   * +:name+ provides a descriptive title for the course
+#   * +:credit_hours+ provides the number of credit hours counted toward degree
+#
 class Course < ActiveRecord::Base
   validates :course_number, presence: true, numericality: true
   validates :instructional_unit, presence: true
