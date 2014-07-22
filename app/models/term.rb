799974546c9d2c9eeb31cea3d1382cc73b7df1a3
gaurimishra <gaurimishra@users.noreply.github.com>
2014-07-22 14:53:18 -0400

    fixed issue #10

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,10 +1,10 @@
 # The +Term+ class represents a single term of a section of a course offered by a College
-
-# Term Attributes are stored as follows: 
-
-# * +:name+ is stored as string and represents the name of the term 
-# * +:start_date+ is stored as date and represents the start date of the section for the term
-# * +:end_date+ is stored as date and represents the end date of the section for the term
+#
+#  Attributes are stored as follows: 
+#
+#   * +:name+ is stored as string and represents the name of the term 
+#   * +:start_date+ is stored as date and represents the start date of the section for the term
+#   * +:end_date+ is stored as date and represents the end date of the section for the term
 
 
 class Term < ActiveRecord::Base
