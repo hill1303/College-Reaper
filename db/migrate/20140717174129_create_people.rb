486c88902aa4aafbe4b29f4c552458e9054fbbd8
lotzi11 <lotzi11@users.noreply.github.com>
2014-07-23 07:37:13 -0700

    Added Defaults to Columns with validations and made it they cannot be Null too

--- db/migrate/20140717174129_create_people.rb
+++ db/migrate/20140717174129_create_people.rb
@@ -2,17 +2,17 @@ class CreatePeople < ActiveRecord::Migration
   def change
     create_table :people do |t|
       # The person's name
-      t.text :name
+      t.text :name, default: '', null: false
 
       # Date of Birth of the person
-      t.date :dob
+      t.date :dob, default: Date.new(2014, 1, 1), null: false
 
       # Description of the person
-      t.text :sex
+      t.text :sex, :default 'N', null: false
       t.text :gender
 
       # Formal information on the person
-      t.text :pronoun
+      t.text :pronoun, default: 'their', null:  false
       t.text :title
       t.text :suffix
 
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -18,25 +18,16 @@ describe Person do
       expect(person.attributes).to include('name')
     end
 
-    it 'the presence of :name attribute' do
-      should validate_presence_of :name
-    end
 
     it 'includes a :dob attribute' do
       expect(person.attributes).to include('dob')
     end
 
-    it 'the presence of :dob attribute' do
-      should validate_presence_of :dob
-    end
 
     it 'includes a :sex attribute' do
       expect(person.attributes).to include('sex')
     end
 
-    it 'the presence of :sex attribute' do
-      should validate_presence_of :sex
-    end
 
     it 'includes a :gender attribute' do
       expect(person.attributes).to include('gender')
@@ -46,9 +37,6 @@ describe Person do
       expect(person.attributes).to include('pronoun')
     end
 
-    it 'the presence of :pronoun attribute' do
-      should validate_presence_of :pronoun
-    end
 
     it 'includes a :title attribute' do
       expect(person.attributes).to include('title')
@@ -56,6 +44,24 @@ describe Person do
 
     it 'includes a :suffix attribute' do
       expect(person.attributes).to include('suffix')
+    end
+  end
+
+  context 'validates' do  
+    it 'the presence of :name attribute' do
+      should validate_presence_of :name
+    end
+    
+    it 'the presence of :dob attribute' do
+      should validate_presence_of :dob
+    end
+
+    it 'the presence of :sex attribute' do
+      should validate_presence_of :sex
+    end
+
+    it 'the presence of :pronoun attribute' do
+      should validate_presence_of :pronoun
     end
   end  
 end
