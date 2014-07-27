990f09536d72d7504d5a9473f9f04f9a2bd0f30b
gaurimishra <gaurimishra@users.noreply.github.com>
2014-07-27 17:38:46 -0400

    Changed .string fields to .text

--- db/migrate/20140717155738_create_terms.rb
+++ db/migrate/20140717155738_create_terms.rb
@@ -2,7 +2,7 @@ class CreateTerms < ActiveRecord::Migration
   def change
     create_table :terms do |t|
       # General Term info
-      t.string :name,  null:false, default: ''
+      t.text :name,  null:false, default: ''
       t.date :start_date, null:false, default: Date.new(2014, 1, 10)
       t.date :end_date, null:false, default: Date.new(2014, 4, 30)
 
