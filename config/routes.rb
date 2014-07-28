9cfbe860ff04ab0b58fe1f8b9b1f8a357374e528
gaurimishra <gaurimishra@users.noreply.github.com>
2014-07-27 20:09:01 -0400

    fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # Routes to the home page and sets is as default
-  #root 'public#home'
-  root 'autocomplete#auto'
+  root 'public#home'
 
   # Adds routes for Devise on the user model
   devise_for :user
