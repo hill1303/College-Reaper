11c015654d4b1c37edc709589504d9701544e4cc
lotzi11 <lotzi11@users.noreply.github.com>
2014-07-29 18:01:33 -0400

    added foundtion css and scss folders

--- db/migrate/20140723161625_create_completions.rb
+++ db/migrate/20140723161625_create_completions.rb
@@ -1,7 +1,7 @@
 class CreateCompletions < ActiveRecord::Migration
   def change
     create_table :completions do |t|
-      # Record a grade for each relationship
+      # Records a letter grade for each relationship
       t.text :grade, limit: 1, null: false, default: 'W'
 
       # Foreign key relationships
