0132fd2a1a0be02d26c354082bb25d49bb869c22
gaurimishra <gaurimishra@users.noreply.github.com>
2014-07-29 14:52:31 -0400

    More data added to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,26 +30,269 @@ courses = Course.create([{
                              name: 'Software 1',
                              credit_hours: 4
                          },
+                         
+                          {
+                             instructional_unit: 'CSE',
+                             course_number: '2501',
+                             name: 'Ethics',
+                             credit_hours: 1
+                         },
+
+                         {
+                             instructional_unit: 'CSE',
+                             course_number: '2321',
+                             name: 'Foundations 1',
+                             credit_hours: 3
+                         },
+
+                         {
+                             instructional_unit: 'CSE',
+                             course_number: '3241',
+                             name: 'Database',
+                             credit_hours: 3
+                         },
+
+                         {
+                             instructional_unit: 'CSE',
+                             course_number: '3231',
+                             name: 'Software Eng',
+                             credit_hours: 3
+                         },
+
+                         {
+                             instructional_unit: 'CSE',
+                             course_number: '3321',
+                             name: 'Formal Lnaguages',
+                             credit_hours: 3
+                         },
+                         {
+                             instructional_unit: 'CSE',
+                             course_number: '3341',
+                             name: 'Programming Languages',
+                             credit_hours: 3
+                         },
+
+                         {
+                             instructional_unit: 'CSE',
+                             course_number: '3421',
+                             name: 'Architecture',
+                             credit_hours: 3
+                         },
+
+                          {
+                             instructional_unit: 'CSE',
+                             course_number: '3461',
+                             name: 'Networking',
+                             credit_hours: 3
+                         },
+
+                          {
+                             instructional_unit: 'CSE',
+                             course_number: '3521',
+                             name: 'Artificial Intelligence',
+                             credit_hours: 3
+                         },
+
+                          {
+                             instructional_unit: 'CSE',
+                             course_number: '3541',
+                             name: 'Graphics',
+                             credit_hours: 3
+                         },
+                      
+                   {
+                        #  instructional_unit: 'CSE',
+                        #    course_number: '3901',
+                        #    name: 'Project: Web Apps',
+                        #    credit_hours: 4
+                        # },
+                        #  {
+                        #     instructional_unit: 'CSE',
+                        #   course_number: '5901',
+                        #    name: 'Capstone',
+                        #     credit_hours: 4
+                        #},
+                         
+
+                          {
+                             instructional_unit: 'Math',
+                             course_number: '3345',
+                             name: 'Foundations of Higher Math',
+                             credit_hours: 3
+                         },
+
                          {
                              instructional_unit: 'MATH',
                              course_number: '1151',
-                             name: 'Calculus 1',
+                             name: 'Calculus I',
                              credit_hours: 5
                          },
+
+                          {
+                             instructional_unit: 'MATH',
+                             course_number: '1152',
+                             name: 'Calculus II',
+                             credit_hours: 5
+                         },
+
+                          {
+                             instructional_unit: 'STAT',
+                             course_number: '3470',
+                             name: 'Probability & Statistics',
+                             credit_hours: 3
+                         },
+
+                           {
+                             instructional_unit: 'ECE',
+                             course_number: '2000',
+                             name:'Electrical &  Computer Engineering I',
+                             credit_hours: 4
+                         },
+
+
                          {
                              instructional_unit: 'PHYSICS',
-                             course_number: '1251',
+                             course_number: '1250',
                              name: 'Mechanical Physics',
                              credit_hours: 5
-                         }
-                            
-                              ])
+                         },
+
+                           {
+                             instructional_unit: 'ENGLISH',
+                             course_number: '1110',
+                             name: 'Writing I',
+                             credit_hours: 3
+                         },
+
+                          {
+                             instructional_unit: 'ENGLISH',
+                             course_number: '2367',
+                             name: 'Writing II',
+                             credit_hours: 3
+                         },
+
+                          {
+                             instructional_unit: 'ENGLISH',
+                             course_number: '3398',
+                             name: 'Writing III',
+                             credit_hours: 3
+                         },
+
+                          {
+                             instructional_unit: 'SWAHILI',
+                             course_number: '1101',
+                             name: 'Swahili I',
+                             credit_hours: 4
+                         },
+
+                         {
+                             instructional_unit: 'SWAHILI',
+                             course_number: '1102',
+                             name: 'Swahili II',
+                             credit_hours: 4
+                         },
+
+                         {
+                             instructional_unit: 'SWAHILI',
+                             course_number: '1103',
+                             name: 'Swahili III',
+                             credit_hours: 4
+                         },
+
+
+                         {
+                             instructional_unit: 'BIOLOGY',
+                             course_number: '1113',
+                             name: 'Biology I',
+                             credit_hours: 4
+                         },
+
+
+                         {
+                             instructional_unit: 'BIOLOGY',
+                             course_number: '1114',
+                             name: 'Biology II',
+                             credit_hours: 4
+                         },
+
+
+                         {
+                             instructional_unit: 'CHEMISTRY',
+                             course_number: '1210',
+                             name: 'Chemistry I',
+                             credit_hours: 5
+                         },
+
+                           {
+                             instructional_unit: 'CHEMISTRY',
+                             course_number: '1220',
+                             name: 'Chemistry II',
+                             credit_hours: 5
+                         },
+
+                         {
+                             instructional_unit: 'SOCIOLOGY',
+                             course_number: '2210',
+                             name: 'Sociological Aspects of Deviance',
+                             credit_hours: 3
+                         },
+
+                         {
+                             instructional_unit: 'HISTORY',
+                             course_number: '2204',
+                             name: 'Modern European History',
+                             credit_hours: 3
+                         },
+
+                         {
+                             instructional_unit: 'PSYCHOLOGY',
+                             course_number: '3371',
+                             name: ' Language and the Mind',
+                             credit_hours: 3
+                         },
+
+                          {
+                             instructional_unit: 'CLASSICS',
+                             course_number: '2220',
+                             name: 'Greek and Roman Mythology',
+                             credit_hours: 3
+                         },
+
+                          {
+                             instructional_unit: 'FILM STUDIES',
+                             course_number: '2270',
+                             name: 'Introduction to Theater',
+                             credit_hours: 5
+                         },
+
+                         {
+                             instructional_unit: 'ECONOMICS',
+                             course_number: '3820',
+                             name: 'Labor Economics',
+                             credit_hours: 3
+                         },
+
+                          {
+                             instructional_unit: 'PHILOSOPHY',
+                             course_number: '2367',
+                             name: 'Social and Moral Problems in the U.S.',
+                             credit_hours: 3
+                         },
+
+                          {
+                             instructional_unit: 'GEOGRAPHY',
+                             course_number: '2400',
+                             name: 'Economic and Social Geography',
+                             credit_hours: 3
+                         },
+
+                        ])
 
 locations = Location.create([{
                                  name: 'Hagerty Hall',
                                  street1: 'Street Name',
                                  street2: nil,
-                                 city: 'Anytown',
+                                 city: 'Columbus',
                                  state: 'OH NO',
                                  zip: '43210',
                                  latlong: 'POINT(39.998479 -83.01019)'
@@ -58,7 +301,7 @@ locations = Location.create([{
                                  name: 'Dreese Lab',
                                  street1: 'Street Name',
                                  street2: nil,
-                                 city: 'Anytown',
+                                 city: 'Columbus',
                                  state: 'OH NO',
                                  zip: '43210',
                                  latlong: 'POINT(40.002241, -83.015965)'
@@ -67,7 +310,7 @@ locations = Location.create([{
                                  name: 'Caldwell Lab',
                                  street1: 'Street Name',
                                  street2: nil,
-                                 city: 'Anytown',
+                                 city: 'Columbus',
                                  state: 'OH NO',
                                  zip: '43210',
                                  latlong: 'POINT(40.002471, -83.015145)'
@@ -76,7 +319,7 @@ locations = Location.create([{
                                  name: 'Fontana Lab',
                                  street1: 'Street Name',
                                  street2: nil,
-                                 city: 'Anytown',
+                                 city: 'Columbus',
                                  state: 'OH NO',
                                  zip: '43210',
                                  latlong: 'POINT(40.003548, -83.012501)'
@@ -85,7 +328,7 @@ locations = Location.create([{
                                  name: 'Jennings Hall',
                                  street1: 'Street Name',
                                  street2: nil,
-                                 city: 'Anytown',
+                                 city: 'Columbus',
                                  state: 'OH NO',
                                  zip: '43210',
                                  latlong: 'POINT(39.996793, -83.015366)'
@@ -93,9 +336,9 @@ locations = Location.create([{
 
 section_params = Array.new
 
-50.times do |i|
+300.times do |i|
   time_delta = 60 * rand(60..120)
-  repeat_patterns = %w(MWF TR TWRF F)
+  repeat_patterns = %w(MWF TR TWRF F WF MW M T W R MTWR)
 
   section_params.push({
       room: '10' << i.to_s,
