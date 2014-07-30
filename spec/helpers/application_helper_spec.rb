c0b5a6e57aee9d1dc2b97c5c1c7762fee6c45c77
gaurimishra <gaurimishra@users.noreply.github.com>
2014-07-30 11:02:30 -0400

    Specs for helper methods added

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+describe ApplicationHelper do
+  describe "#autocompleted_course_to_id" do
+    it "displays" do
+     
+    end
+  end
+  describe "#bias_slider" do
+    it "displays" do
+     
+    end
+  end
+end
